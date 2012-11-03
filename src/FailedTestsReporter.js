7a3b97e6a533b4db39f34e08fa77364d3b3f24b5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2012-11-03 13:26:41 -0400

    jslint

--- src/FailedTestsReporter.js
+++ src/FailedTestsReporter.js
@@ -12,9 +12,8 @@ var incomplete = clc.yellowBright;
 
 function _log(tests) {
 	console.assert(tests, "tests is undefined");
-	
-	function printResult(info, percent, good, total, failed, test)
-	{
+
+	function printResult(info, percent, good, total, failed, test) {
 		var status = (failed > 0 ? "FAIL" : "PASS");
 		var color = (failed > 0 ? fail : success);
 		if (test) {
@@ -25,21 +24,21 @@ function _log(tests) {
 				total = test.expected;
 			}
 		}
-		
+
 		if (status === "PASS") {
 			return;
 		}
-		
+
 		var message = "";
-		
+
 		if (test && test.module) {
 			message = sprintf("%-70s : %3d%% (%d / %d)", test.module + ': ' + info, percent, good, total, failed);
 		} else {
 			message = sprintf("%-70s : %3d%% (%d / %d)", info, percent, good, total, failed);
 		}
-		
+
 		var fullMessage = sprintf("%-90s : %s", message, status);
-		
+
 		var color = (failed > 0 ? fail : success);
 		if (test) {
 			if (test.hasCrashed) {
@@ -65,14 +64,15 @@ function _log(tests) {
 			var good = test.assertions - test.broken;
 			var percent = (test.assertions === 0 ? 100 : good / test.assertions * 100);
 			printResult(test.name, percent, good, test.assertions, test.broken, test);
-			
+
 			totalTests += 1;
 			if (test.broken > 0 || test.hasCrashed) {
 				totalFailedTests += 1;
 				failedTests.push(test);
 			}
 		}
-	};
+	}
+	;
 
 	console.log(centerMessage(lineLength));
 	var good = totalTests - totalFailedTests;
@@ -81,7 +81,7 @@ function _log(tests) {
 	console.assert(totalTests >= 0, "invalid total tests", totalTests);
 	console.assert((totalFailedTests >= 0) && (totalFailedTests <= totalTests), "invalid failed tests", totalFailedTests, "total", totalTests);
 	printResult("passed tests", percent, good, totalTests, totalFailedTests);
-	
+
 	return totalFailedTests;
 }
 
--- src/Reporter.js
+++ src/Reporter.js
@@ -12,33 +12,40 @@ var incomplete = clc.yellowBright;
 
 function _log(tests) {
 	console.assert(tests, "tests is undefined");
-	
-	function printResult(info, percent, good, total, failed, test)
-	{
+
+	function printResult(info, percent, good, total, failed, test) {
+		console.assert(typeof info === "string", "info should be a string", info);
+		console.assert(!isNaN(percent), "percent should be a number", percent);
+		console.assert(!isNaN(good), "good is not a number", good);
+		console.assert(!isNaN(total), "total is not a number", total);
+		console.assert(!isNaN(failed), "failed is not a number", failed);
+
 		var status = (failed > 0 ? "FAIL" : "PASS");
-		var color = (failed > 0 ? fail : success);
 		if (test) {
 			if (test.hasCrashed) {
 				status = "CRASH";
-			} else if ((typeof test.expected !== "undefined") && (test.expected != test.assertions)) {
+			} else if ((test.expected !== "undefined") && test.expected && (test.expected !== test.assertions)) {
 				status = "INCOMPLETE";
 				total = test.expected;
+				console.assert(!isNaN(total), "total expected for incomplete is not a number", total, "test", test.name);
 			}
 		}
-		
+
+		var message = "";
 		if (test && test.module) {
+			console.assert(typeof test.module === "string", "test module should be a string", test.module);
 			message = sprintf("%-70s : %3d%% (%d / %d)", test.module + ': ' + info, percent, good, total, failed);
 		} else {
 			message = sprintf("%-70s : %3d%% (%d / %d)", info, percent, good, total, failed);
 		}
-		
+
 		var fullMessage = sprintf("%-90s : %s", message, status);
-		
+
 		var color = (failed > 0 ? fail : success);
 		if (test) {
 			if (test.hasCrashed) {
 				color = crash;
-			} else if ((typeof test.expected !== "undefined") && (test.expected != test.assertions)) {
+			} else if ((test.expected !== "undefined") && test.expected && (test.expected !== test.assertions)) {
 				color = incomplete;
 			}
 		}
@@ -52,30 +59,32 @@ function _log(tests) {
 	var totalTests = 0;
 	var totalFailedTests = 0;
 	var failedTests = [];
+	var testName;
 
-	for (var testName in tests) {
+	for (testName in tests) {
 		if (tests.hasOwnProperty(testName)) {
 			var test = tests[testName];
 			var good = test.assertions - test.broken;
 			var percent = (test.assertions === 0 ? 100 : good / test.assertions * 100);
+			console.assert(test.assertions >= 0, "invalid number of assertions", test.assertions, "test", testName);
 			printResult(test.name, percent, good, test.assertions, test.broken, test);
-			
+
 			totalTests += 1;
 			if (test.broken > 0 || test.hasCrashed) {
 				totalFailedTests += 1;
 				failedTests.push(test);
 			}
 		}
-	};
+	}
 
 	console.log(centerMessage(lineLength));
-	var good = totalTests - totalFailedTests;
-	var percent = (totalTests === 0 ? 100 : good / totalTests * 100);
+	var goodTests = totalTests - totalFailedTests;
+	var percentPassed = (totalTests === 0 ? 100 : goodTests / totalTests * 100);
 
 	console.assert(totalTests >= 0, "invalid total tests", totalTests);
 	console.assert((totalFailedTests >= 0) && (totalFailedTests <= totalTests), "invalid failed tests", totalFailedTests, "total", totalTests);
-	printResult("passed tests", percent, good, totalTests, totalFailedTests);
-	
+	printResult("passed tests", percentPassed, goodTests, totalTests, totalFailedTests);
+
 	return totalFailedTests;
 }
 
