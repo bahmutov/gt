abfa50b93ae9893e2ace70cfd6b66d8603147203
gleb bahmutov <gleb.bahmutov@gmail.com>
2013-01-08 22:00:22 -0500

    not using global args object

--- gt.js
+++ gt.js
@@ -9,27 +9,27 @@ try {
 }
 
 // grab command line arguments
-(function() {
+var arguments = (function() {
 	var optimist = require("optimist");
-	args = optimist.usage("JS unit testing and coverage in a single shot.\nUsage: $0")
-			.default({
-				log: 1,
-				report: 0,
-				help: 0,
-				cover: "cover",
-				xml: null,
-				colors: true,
-				module: []
-			}).alias('l', 'log').alias('r', 'report').alias('h', 'help').alias('c', 'cover')
-			.string("cover").string("xml")
-			.boolean("colors")
-			.describe('l', "log level, 0 - debug, 1 - normal, 2 - warnings, 3 - errors only")
-			.describe('r', "report level, 0 - all test results, 1 - skip passed tests")
-			.describe("cover", "output folder with coverage")
-			.describe("xml", "output JUnit xml filename")
-			.describe('colors', 'use terminal colors for output, might not work with continuous build servers')
-			.describe('module', 'test module to run, can be used multiple times')
-			.argv;
+	var args = optimist.usage("JS unit testing and coverage in a single shot.\nUsage: $0")
+		.default({
+			log: 1,
+			report: 0,
+			help: 0,
+			cover: "cover",
+			xml: null,
+			colors: true,
+			module: []
+		}).alias('l', 'log').alias('r', 'report').alias('h', 'help').alias('c', 'cover')
+		.string("cover").string("xml")
+		.boolean("colors")
+		.describe('l', "log level, 0 - debug, 1 - normal, 2 - warnings, 3 - errors only")
+		.describe('r', "report level, 0 - all test results, 1 - skip passed tests")
+		.describe("cover", "output folder with coverage")
+		.describe("xml", "output JUnit xml filename")
+		.describe('colors', 'use terminal colors for output, might not work with continuous build servers')
+		.describe('module', 'test module to run, can be used multiple times')
+		.argv;
 
 	if (!module.parent) {
 		if (args.h || args.help || !args._.length) {
@@ -38,18 +38,19 @@ try {
 			process.exit(0);
 		}
 	}
+	return args;
 }());
 
-if (typeof args.module === 'string') {
-	args.module = [args.module];
+if (typeof arguments.module === 'string') {
+	arguments.module = [arguments.module];
 }
 
 var logger = require('optional-color-logger');
-logger.init(args);
+logger.init(arguments);
 
 var sure = require('./covered');
 console.assert(typeof sure === "object", 'loaded sure module');
 
-sure.init(args);
+sure.init(arguments);
 var failed = sure.run();
 process.exit(failed);
