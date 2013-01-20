9e648269e3d2e52e067c665fbab353ce2450d2b6
gleb bahmutov <gleb.bahmutov@gmail.com>
2013-01-20 18:23:30 -0500

    moved command line options into separate file

---
+++ src/options.js
@@ -0,0 +1,43 @@
+var optimist = require("optimist");
+
+function getArguments() {
+	var args = optimist.usage("JS unit testing and coverage in a single shot.\nUsage: $0")
+		.default({
+			log: 1,
+			report: 0,
+			help: 0,
+			cover: "cover",
+			xml: null,
+			colors: true,
+			module: [], // all files with test and code
+			output: false,
+			watch: false,
+			jsunity: false,
+			doh: false
+		}).alias('l', 'log').alias('r', 'report').alias('h', 'help').alias('c', 'cover')
+		.string("cover").string("xml")
+		.boolean("colors")
+		.describe('l', "log level, 0 - debug, 1 - normal, 2 - warnings, 3 - errors only")
+		.describe('r', "report level, 0 - all test results, 1 - skip passed tests")
+		.describe("cover", "output folder with coverage")
+		.describe("xml", "output JUnit xml filename")
+		.describe('colors', 'use terminal colors for output, might not work with continuous build servers')
+		.describe('module', 'test module to run, can be used multiple times')
+		.boolean('output').describe('output', 'do not hide standard and warning console output messages')
+		.alias('w', 'watch').boolean('watch')
+		.describe('watch', 'watch files for changes, rerun the unit tests')
+		.boolean('jsunity').describe('jsunity', 'unit tests follow jsunity rules')
+		.boolean('doh').describe('doh', 'unit tests follow Dojo DOH syntax (including define)')
+		.argv;
+
+	if (!module.parent) {
+		if (args.h || args.help || !args._.length) {
+			optimist.showHelp();
+			console.log(args);
+			process.exit(0);
+		}
+	}
+	return args;
+}
+
+exports.run = getArguments;
