b6556126eae00983110ad12a0e6bbd3f1fdcbae9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-01-16 11:39:47 -0500

    case sensitive names

--- tests/small/errorInAsyncTest.js
+++ tests/small/errorInAsyncTest.js
@@ -2,7 +2,12 @@ gt.module('error in async test handled');
 
 var path = require('path');
 var gtPath = path.join(__dirname, '../../gt.js');
-var testFile = path.join(__dirname, 'errorinAsync.js');
+var testFile = path.join(__dirname, 'errorInAsync.js');
+var fs = require('fs');
+
+gt.test('file exists', function () {
+  gt.ok(fs.existsSync(testFile), 'file', testFile, 'exists');
+});
 
 gt.async('expect error', 1, function () {
   gt.exec('node', [gtPath, testFile], 1, 'gt returns 1');
